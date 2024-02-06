0c4282d32b243966a10557a65c21cb17aa8b69f1
sisicodes <138730969+sisicodes@users.noreply.github.com>
2024-02-06 13:50:06 -0500

    fix:fix uncover cancel functionality

--- assets/twice.js
+++ assets/twice.js
@@ -1,64 +1,109 @@
 class Board {
+    
+    static imgList = ["url('assets/images/idols/sana1.jpg')", "url('assets/images/idols/mina1.jpg')", "url('assets/images/idols/tzuyu1.jpg')"];
+    static currentImg = this.imgList[0];
+    static photoCounter = 0;
+    //static keep = true;
+    static shuffledArray = [];
+    static counter = 0;
 
     static initializeBoard() {
-        this.image=document.getElementById('container').style.backgroundImage;
-        this.image = "url('assets/images/idols/sana1.jpg')";
-        this.container = document.getElementById('container');
-        this.container.style.backgroundImage =  "url('assets/images/idols/sana1.jpg')";
+        //Board.keep = true;
+        this.updatePhoto();
         console.log('initialize')
+
+    }
+
+    static updateBoard () {
+        
+    };
+
+    static updatePhoto() {
+        Board.currentImg = this.imgList[this.photoCounter];
+        console.log(`current image: ${Board.currentImg}`)
+        Board.container = document.getElementById('container');
+        Board.container.style.backgroundImage =  this.currentImg;
+        Board.photoCounter++;
 
     }
     static createArray() {
         let numberArray = []
         for (let i = 1; i<26; i++) {
             numberArray.push(i);
-            console.log(numberArray)
         }
         return numberArray
     }
 
     static shuffleArray(array) {
-        const index = array.indexOf(17); //hardcoded for now
-        array.splice(index,1); //opportunity here to protect if it is not found in the array
-        let shuffleArray = [];
+        //const index = array.indexOf(17); //hardcoded for now
+        //array.splice(index,1); //opportunity here to protect if it is not found in the array
+        //let shuffleArray = [];
+        Board.shuffledArray = [];
         let shuffleIndex;
         let shuffleValue;
         for (let i=0; 1<array.length+1; i++) {
             shuffleIndex = Math.floor(Math.random() * array.length);
             shuffleValue = array.splice(shuffleIndex,1);
-            shuffleArray.push(shuffleValue[0]);
+            Board.shuffledArray.push(shuffleValue[0]);
         };
-        return shuffleArray;
+        return Board.shuffledArray;
     }
 
     static uncoverEach(loc) {
-        const picDiv = document.getElementById(loc.toString());
-        picDiv.classList.add('uncovered');
+            const picDiv = document.getElementById(loc.toString());
+            picDiv.classList.add('uncovered');
+        
+      
 
     }
 
-    static uncoverAll() {
-        let array = this.createArray();
-        this.shuffleArray = this.shuffleArray(array);
-        for (const element of this.shuffleArray) {
-            setTimeout(this.uncoverEach,1000*(this.shuffleArray.indexOf(element)+1),element)
+    static recoverAll(loc) {
+        const allPicDiv = document.getElementById('container').getElementsByTagName('*');
+       //Board.keep = true;
+        Board.cancelUncover();
+        for (const element of allPicDiv) {
+            element.classList.remove('uncovered');
         };
+    };
+
+    static uncoverAll() {
+        let array = Board.createArray();
+        Board.shuffledArray = Board.shuffleArray(array)
+        Board.un  = setInterval(Board.uncoverAll1,1000);
+
+
+
+    }
+
+    static uncoverAll1() {
+        Board.uncoverEach(Board.shuffledArray[Board.counter]);
+        Board.counter++;
+        console.log('in uncover helper');
+        if (Board.counter==25) {
+            Board.cancelUncover();
+        }
     }
 
     static uncoverImm() {
-        for (const element of this.shuffleArray) {
+        for (const element of Board.shuffledArray) {
             this.uncoverEach(element);
         }
+        Board.cancelUncover();
     }
+
+    static cancelUncover() {
+        clearInterval(Board.un);
+    };
 }
 
 class newGame {
 
 
     click() {
-        this.button.classList.add('none');
+        //this.button.classList.add('none');
         this.clicked = true;
         Board.uncoverAll();
+        stopwatchSingleton.resetTimer();
         stopwatchSingleton.startTimer();
     }
 
@@ -75,7 +120,12 @@ class Ready extends newGame {
         ready.addEventListener('mouseover', this.hover.bind(this));
         ready.addEventListener('mouseleave', this.unhover.bind(this));
         ready.addEventListener('click', this.click.bind(this));
+        ready.addEventListener('click', this.readyClick.bind(this));
         Board.initializeBoard();
+    }
+
+    readyClick() {
+        this.button.classList.add('none');
     }
 
     hover() {
@@ -94,9 +144,26 @@ class Ready extends newGame {
 class Next extends newGame {
     constructor() {
         super();
-        this.button = document.getElementById('next')
         this.clicked = false;
-        next.addEventListener('click', this.click.bind(this));
+        this.button = document.getElementById('next');
+        next.addEventListener('click', this.nextClick.bind(this));
+        //next.addEventListener('click', this.click.bind(this));
+    }
+
+    nextClick() {
+        console.log('next click');
+        Board.initializeBoard();
+        Board.recoverAll();
+        Board.uncoverAll();
+        stopwatchSingleton.resetTimer();
+        stopwatchSingleton.startTimer();
+        this.button.classList.add('hidden');
+        submit.submit.classList.remove('hidden');
+        submit.input.classList.remove('hidden');
+    }
+
+    nextDeactivated() {
+        next.removeEventListener('click', this.click.bind(this));
     }
 }
 
@@ -104,24 +171,27 @@ class Submit {
     constructor() {
         this.submit = document.getElementById('submit');
         submit.addEventListener('click', this.isCorrect.bind(this));
+        this.input = document.getElementById('answerText');
     }
 
     correctAnswer() {
         stopwatchSingleton.stopTimer();
         console.log('correct answer')
         Board.uncoverImm();
-        this.submit.id= 'next';
-        this.next = new Next();
+        next.button.classList.remove('hidden');
+        this.input.value = '';
+        this.submit.classList.add('hidden');
+        this.input.classList.add('hidden');
+
     }
 
     isCorrect() {
         this.answer = document.getElementById('answerText').value;
         this.possible = ['tzuyu', 'mina', 'sana'];
-        console.log(Board.image)
+        console.log(`current image is ${Board.currentImg}`)
         if (this.possible.includes(this.answer.toLowerCase())) {
             console.log('an idol!');
-            if (Board.image.includes(this.answer.toLowerCase())) {
-                console.log('right answer!');
+            if (Board.currentImg.includes(this.answer.toLowerCase())) {
                 this.correctAnswer();
             }
         } else {
@@ -152,7 +222,6 @@ class Stopwatch {
 
         } else if (parseInt(this.centi)<99) {
             this.centi = (parseInt(this.centi)+1).toString();
-            console.log('in else if')
             if (this.centi.length==1) {
                 this.centi = '0' +this.centi;
             }
@@ -165,7 +234,7 @@ class Stopwatch {
         console.log('start timer');
         let i = 1;
         this.on = setInterval(this.incrementTime.bind(this),10);
-        setTimeout(this.stopTimer.bind(this), 25000);
+        setTimeout(this.stopTimer.bind(this), 100000);
 
     }
 
@@ -174,7 +243,7 @@ class Stopwatch {
         this.on = undefined;
     }
 
-    refreshTimer() {
+    resetTimer() {
         this.second = '00';    
         this.centi = '00';
     }
@@ -184,4 +253,5 @@ class Stopwatch {
 
 ready = new Ready();
 submit = new Submit();
+next = new Next();
 stopwatchSingleton = new Stopwatch();
